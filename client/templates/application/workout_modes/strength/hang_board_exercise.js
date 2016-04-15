586ee15ce484a3e8d6568f061de34133545fef98
brad <brad@hillsidesc.org>
2016-04-15 11:50:39 -0700

    Can now add sets without page refresh problems. DOM element focus is retained and reactivity appears to be retained. Remember that helpers are automatically reactive, so if trouble with that, try moving data definitions and Collection.find(...) to helpers

--- client/templates/application/workout_modes/strength/hang_board_exercise.js
+++ client/templates/application/workout_modes/strength/hang_board_exercise.js
@@ -1,18 +1,22 @@
 Template.hangBoardExercise.onCreated(function(){
     /***in this block, 'this.' refers to the current template instance.***/
-    //console.log("logging hangBoardExerice onCreeated and 'this' is: " + this);
+    console.log("logging hangBoardExerice onCreeated and 'this' is: " + this);
     //showExercise is used to toggle .exerciseFormAttributes to collape/show the exercise
     this.showExercise = new ReactiveVar( true );
     mysteryObject = this;
-    this.data.exercise =Exercises.findOne({ _id: this.data.exId});    
+    
+    //Meteor.subscribe('single_ex_collection',this.data.exId);
+    //this.data.exercise =Exercises.findOne({ _id: this.data.exId});    
     //console.log(this);
 
  });
 
 /******************** hangBoardExercise Herlpers ***********************/
 Template.hangBoardExercise.helpers({
+
     sets: function(){
-        return Template.instance().data.exercise.sets;
+        exercise = Exercises.findOne({ _id: Template.instance().data.exId});
+        return exercise.sets;
     },
     showExercise: function(){
         return Template.instance().showExercise.get();
@@ -52,10 +56,25 @@ Template.hangBoardExercise.events({
         template.showExercise.set( !template.showExercise.get() ) // toggle value of show notes
     },
     'click .new_set': function(event, template){
-        event.preventDefault();
         console.log("Template.instance().data.exID: "+Template.instance().data.exId);
 
-        
+       event.preventDefault();
+       console.log(Session.get('newHB_id'));
+       exIndex = Template.instance().data.exIndex; 
+       exId = Template.instance().data.exId;
+       console.log('exIndex:'+exIndex);
+       update = {$push:{}}; 
+       update.$push['sets'] = {field:{ach:0, felt:0, goal:0}} 
+       //template.newSet.set( !template.newSet.get() ); 
+       //console.log(Template.parentData()['sets']);
+       console.log(update); 
+       Exercises.update({_id: exId },update,
+           function(error){
+               if (error){
+                   console.log(error);
+               }
+
+           });       
     }
     
 });
